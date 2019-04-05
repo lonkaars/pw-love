4f652b3782b153e7bd2d91e530e184f5d45d0818
Loekaars <l.leblansch@gmail.com>
2019-04-05 17:06:28 +0200

    gErt 2

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var Youtube = require('simple-youtube-api')
-const youtube = new Youtube("AIzaSyD7PHYZP7FTTslvYkiQ0Jk17nTwskw_ELQ");
-
 var PWCount;
 var TCount;
 var diff;
 
+const APIKEYSARR = ['AIzaSyD7PHYZP7FTTslvYkiQ0Jk17nTwskw_ELQ', 'AIzaSyBAvxoyI1rFpDTdyYkiiJH171aF3n-D6Rw', 'AIzaSyA-rZ-MEWGZpKFEpYOwMZXSGxVOXHwqUVE', 'AIzaSyBwVQcM6FO8zdgNJZa0Kyy_fxefyPkx-MI']
+var APIKEY = APIKEYSARR[0]
+var APIKEYNUM = 0;
+const TSERIESID = 'UCq-Fj5jknLsUf-MWSy4_brA'
+const PWID = 'UCHSgjpeDxmp6jsHUmXDHNQQ'
+
 $(() => {
     setInterval(() => {
         updateVals()
-    }, 1000);
+    }, 2000);
 })
 
 function updateCount(count, text) {
@@ -16,20 +19,42 @@ function updateCount(count, text) {
 }
 
 function updateVals() {
-    youtube.getChannelByID('UCq-Fj5jknLsUf-MWSy4_brA')
-        .then(channel => {
-            if (channel) {
-                updateCount(2, channel.subscriberCount)
-            }
-        })
-        .catch(console.error);
-    youtube.getChannelByID('UCHSgjpeDxmp6jsHUmXDHNQQ')
-        .then(channel => {
-            if (channel) {
-                updateCount(1, channel.subscriberCount)
-                console.log(channel.subscriberCount)
-                console.log(channel)
-            }
-        })
-        .catch(console.error);
+    try {
+        TCount = JSON.parse(httpGet(makeURL(TSERIESID, APIKEY))).items[0].statistics.subscriberCount;
+        updateCount(2, format(TCount))
+        PWCount = JSON.parse(httpGet(makeURL(PWID, APIKEY))).items[0].statistics.subscriberCount;
+        updateCount(1, format(PWCount))
+        diff = Math.abs(TCount - PWCount);
+        updateCount(3, format(diff))
+        $('#quotaReached').removeClass('show')
+    } catch (error) {
+        nextKey()
+        $('#quotaReached').addClass('show')
+        // console.log(error);
+    }
+}
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function makeURL(channelID, APIKey) {
+    return `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${APIKey}`
+}
+
+function format(n) {
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+function nextKey() {
+    if(APIKEYNUM + 1 < APIKEYSARR.length){
+        APIKEYNUM++
+        APIKEY = APIKEYSARR[APIKEYNUM]
+    } else {
+        APIKEYNUM = 0;
+        APIKEY = APIKEYSARR[0]
+    }
 }
